d1dc8202756d68c18c967701ba98cf0d1bbd1e9b
Sachindra Fernando <sachindrafernando3@gmail.com>
2025-07-29 22:14:52 +0100

    Initial Commit

---
+++ SwiftConcurrencyTrends/SwiftConcurrencyTrendsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftConcurrencyTrendsApp.swift
+//  SwiftConcurrencyTrends
+//
+//  Created by Sachindra Fernando on 2025-07-29.
+//
+
+import SwiftUI
+
+@main
+struct SwiftConcurrencyTrendsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
